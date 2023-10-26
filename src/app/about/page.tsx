ea2f16a146e93fb65d447b8627fc7c26790290e9
Sanjero20 <gelosantos72@gmail.com>
2023-10-26 12:21:55 +0800

    Initialize new pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+function AboutPage() {
+  return <div>About Me</div>;
+}
+
+export default AboutPage;
---
+++ src/app/learn/page.tsx
@@ -0,0 +1,5 @@
+function LearnPage() {
+  return <div> Memorize Provinces</div>;
+}
+
+export default LearnPage;
