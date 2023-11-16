08fb2da2ece2abe26dc78b310899d6ed42ddca2b
Sanjero20 <gelosantos72@gmail.com>
2023-11-17 00:20:04 +0800

    Add tooltip for hovering on provinces

--- src/app/learn/philippines-map.tsx
+++ src/app/learn/philippines-map.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useCallback } from "react";
-import { MapContainer, GeoJSON } from "react-leaflet";
-import { LatLngBoundsExpression, Layer, LeafletMouseEvent } from "leaflet";
-import { REGIONS } from "@/data/regions";
-
 import "leaflet/dist/leaflet.css";
+
+import { useCallback, useEffect, useState, MouseEvent } from "react";
+import { MapContainer, GeoJSON } from "react-leaflet";
+import { Layer, LeafletMouseEvent, setOptions } from "leaflet";
+import { REGIONS } from "@/data/regions";
 import useRegion from "@/stores/region";
 
 const defaultStyles = {
@@ -14,6 +14,7 @@ const defaultStyles = {
   fillOpacity: "white",
   dashArray: "",
   weight: 1,
+  zIndex: 10,
 };
 
 const highlightStyles = {
@@ -22,29 +23,44 @@ const highlightStyles = {
   fillOpacity: "1",
 };
 
-const philippinesBoundary: LatLngBoundsExpression = [
+const philippinesBoundary = [
   [4.277256, 122.416079],
   [21.33895, 121.721292],
   [3.086835, 116.133513],
   [13.5145, 127.301521],
 ];
 
+const philippinesCenter = [12.8797, 121.774];
+
 function PhilippinesMap() {
   const { selectedRegion } = useRegion();
 
+  const [tooltipContent, setTooltipContent] = useState("");
+  const [tooltipPos, setTooltipPos] = useState({ x: 0, y: 0 });
+
   const highlightFeature = useCallback((e: LeafletMouseEvent) => {
     const layer = e.target;
+
+    const x = e.containerPoint.x + 15;
+    const y = e.containerPoint.y + 5;
+
+    setTooltipPos({ x, y });
+
+    const { ADM2_EN: province } = layer.feature.properties;
+    setTooltipContent(province);
+
     layer.setStyle(highlightStyles);
   }, []);
 
   const resetStyles = useCallback((e: LeafletMouseEvent) => {
+    setTooltipContent("");
     e.target.setStyle(defaultStyles);
   }, []);
 
   const onEachFeature = useCallback(
     (feature: any, layer: Layer) => {
       layer.on({
-        mouseover: highlightFeature,
+        mousemove: highlightFeature,
         mouseout: resetStyles,
       });
     },
@@ -55,11 +71,11 @@ function PhilippinesMap() {
   return (
     <MapContainer
       className="h-full w-full bg-sky-200"
-      center={[12.8797, 121.774]}
+      center={philippinesCenter as any}
       zoom={5}
       minZoom={5.25}
       maxZoom={8}
-      maxBounds={philippinesBoundary}
+      maxBounds={philippinesBoundary as any}
       style={{ background: "#d4f1f9" }}
       attributionControl={false}
       doubleClickZoom={false}
@@ -76,10 +92,22 @@ function PhilippinesMap() {
             data={region as any}
             style={styles as any}
             onEachFeature={onEachFeature}
-            // Render the other style in GeoJSON styles format
           />
         );
       })}
+
+      {tooltipContent && (
+        <div
+          className="absolute rounded bg-black p-2 text-white"
+          style={{
+            top: `${tooltipPos.y}px`,
+            left: `${tooltipPos.x}px`,
+            zIndex: 1000,
+          }}
+        >
+          {tooltipContent}
+        </div>
+      )}
     </MapContainer>
   );
 }
