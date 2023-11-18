ef52341265f1f75afdec9636152766483ce753f0
Sanjero20 <gelosantos72@gmail.com>
2023-11-18 23:00:44 +0800

    Fix button color

--- src/app/(home)/get-started.tsx
+++ src/app/(home)/get-started.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 function GetStartedButton() {
   return (
-    <Button asChild className="w-fit rounded-full px-12">
+    <Button asChild className="w-fit rounded-full bg-white px-12 text-black">
       <Link href={"/learn"}>Get Started</Link>
     </Button>
   );
